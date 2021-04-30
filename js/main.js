fc1a21a0a79a202817dbf78907d9771451a5aa60
sairish2001 <sairish2001@gmail.com>
2021-04-30 15:33:44 +0530

    Editor Window added, with funcitonality of changing colors and font, UI improved a little

--- js/main.js
+++ js/main.js
@@ -5,6 +5,8 @@ var csvdata = [];
 // var imgdata=[];
 
 function addcertificate() {
+    canvas.clear();
+
     var certificate = document.getElementById('certificate');
     certificate.addEventListener('change', handleImage, false);
 
@@ -22,39 +24,6 @@ function addcertificate() {
         }
         reader.readAsDataURL(e.target.files[0]);
     }
-
-
-    // var img = new Image();
-    // img.onload = function () {
-    //     var f_img = new fabric.Image(img);
-    //     canvas.setBackgroundImage(f_img);
-    //     canvas.width=img.width;
-    //     canvas.height=img.height;
-    //     canvas.renderAll();
-    // };
-    // var myDataURL = "pic.png";
-    // img.src = myDataURL;
-
-
-
-    // canvas.renderAll();
-    // canvas.setDimensions({ width: img.width, height: img.height });
-
-    // var canvas = new fabric.Canvas('canvas');
-    // document.getElementById('file').addEventListener("change", function (e) {
-    //     var file = e.target.files[0];
-    //     var reader = new FileReader();
-    //     reader.onload = function (f) {
-    //         var data = f.target.result;
-    //         fabric.Image.fromURL(data, function (img) {
-    //             var oImg = img.set({ left: 0, top: 0, angle: 0, width: 100, height: 100 }).scale(0.9);
-    //             canvas.add(oImg).renderAll();
-    //             var a = canvas.setActiveObject(oImg);
-    //             var dataURL = canvas.toDataURL({ format: 'png', quality: 0.8 });
-    //         });
-    //     };
-    //     reader.readAsDataURL(file);
-    // });
 }
 
 function deleteallobjects() {
@@ -67,34 +36,56 @@ function deleteallobjects() {
 }
 
 function readcsv(input) {
-    deleteallobjects();
-    var file = input.files[0];
-    var reader = new FileReader();
+    var l = String(input.value).split('.');
+    var extension = l[l.length - 1].toLowerCase();
+    if (extension != 'csv') {
+        alert("Only CSV files are compatible");
+        input.value='';
+    } else {
+        deleteallobjects();
+        var file = input.files[0];
+        var reader = new FileReader();
 
-    reader.readAsText(file);
+        reader.readAsText(file);
 
-    reader.onload = function () {
-        var data = reader.result;
-        var ar = [];
-        data = data.replace(/\r/g, "").split(/\n/);
+        reader.onload = function () {
+            var data = reader.result;
+            var ar = [];
+            data = data.replace(/\r/g, "").split(/\n/);
 
-        for (i = 0; i < data.length; i++) {
-            ar[i] = [];
-            var d = data[i].split(',');
-            for (j = 0; j < d.length; j++) {
-                ar[i][j] = d[j];
+            for (i = 0; i < data.length; i++) {
+                ar[i] = [];
+                var d = data[i].split(',');
+                for (j = 0; j < d.length; j++) {
+                    ar[i][j] = d[j];
+                }
             }
-        }
-        csvdata = ar;
-        displaydatafield(csvdata);
-    };
+            csvdata = ar;
+            displaydatafield(csvdata);
+        };
 
-    reader.onerror = function () {
-        // console.log("called")
-        console.log(reader.error);
-    };
+        reader.onerror = function () {
+            console.log(reader.error);
+        };
+    }
 }
+canvas.on('selection:cleared', function (options) {
+    $('#editfield').fadeOut();
+})
+canvas.on('selection:created', function (options) {
+    $('#editfield').fadeIn();
+})
 
+function changeFontStyle(fontvalue) {
+    var obj = canvas.getActiveObject();
+    obj.fontFamily = fontvalue;
+    canvas.renderAll();
+}
+function changeFontColor(colorvalue) {
+    var obj = canvas.getActiveObject();
+    obj.set({ fill: colorvalue })
+    canvas.renderAll();
+}
 
 function displaydatafield(csvdata) {
     var headings = csvdata[0];
@@ -104,6 +95,7 @@ function displaydatafield(csvdata) {
     for (i = 0; i < headings.length; i++) {
         btnids = headings[i] + 'btn';
         deleteids = headings[i] + 'deletebtn';
+        editids = headings[i] + 'editbtn'
         temp += '<tr>' +
             '<td><span>' + headings[i] + '</span></td>' +
             '<td><button type="button" class="btn btn-success" id="' + btnids + '" onclick=\"addtext(\'' + headings[i] + '\'),disablebutton(this.id)\">Add</button></td>' +
@@ -152,16 +144,13 @@ function deleteSelectedObject() {
 function addtext(defaulttext) {
     var n = new fabric.Text(defaulttext, {
         fontFamily: 'Delicious_500',
-        fill: 'white',
+        fill: '#fffdsd',
         fontSize: 90,
-        top: 470,
-        left: 500,
+        top: canvas.height / 2,
+        left: canvas.width / 2,
         id: objcounter
     });
     objcounter++;
-    // console.log(n.left);
-    // console.log(n.top);
-    // console.log(n)
 
     canvas.add(n);
 
@@ -227,7 +216,7 @@ function download(name) {
 //     for (i of imgdata){
 //         zip.file("name", i, { base64: true });
 //     }
-        
+
 //     zip.generateAsync({ type: "blob" })
 //         .then(function (content) {
 //             // see FileSaver.js
